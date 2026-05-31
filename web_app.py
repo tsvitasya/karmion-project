352529e368bea0103f4b7d1bb325ab8eceedb652
tsvitasya <tsvitasya@gmail.com>
2026-05-31 11:08:28 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,16 @@
 from flask import Flask, request, render_template_string
-import kara  # Мы импортируем твой kara.py как модуль
+import sys
+import builtins
+
+# Создаем "заглушку" для input, которая не будет спрашивать, а просто вернет пустоту или число
+def fake_input(prompt=None):
+    return "1"  # Это значение будет подставляться везде, где программа просит ввод
+
+builtins.input = fake_input
+
+# Теперь импортируем твой файл
+import kara
+import кара  # Мы импортируем твой кара.py как модуль
 
 app = Flask(name)
 
